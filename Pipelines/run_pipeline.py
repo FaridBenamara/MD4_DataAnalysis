12a2b596367273df7645688119dddb34f3f14ef2
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2025-03-14 11:33:49 +0100

    Pipeline runnig

---
+++ Pipelines/run_pipeline.py
@@ -0,0 +1,8 @@
+from pipelines.train_pipeline import training_pipeline
+
+if __name__ == "__main__":
+    #Run the pipeline
+    # training_pipeline(data_path='../Stop_Data_2019_to_2022.csv')
+    pass
+
+
